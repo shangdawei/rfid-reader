e9564817b60266da39fb99ed542b11b1f4dff3e4
pacemkr <pacemkr@4c31db07-994d-0410-8d14-35da9c29b421>
2008-05-21 21:55:56 +0000

    Led driver module added. Comparator is correctly sampled and the led responds by lighting up whenever a HIGH level is sampled on the comparator.

--- MPLAB Project/Sampler.asm
+++ MPLAB Project/Sampler.asm
@@ -1,41 +1,26 @@
-	include	"P16F88.INC"
+	#include	<p16f88.inc>
+	
+	extern TurnLedOn
+	extern TurnLedOff
 
 Sampler	code
 
 SampleEncodedBits
 	global SampleEncodedBits
 
-	;setup ports
-	banksel	TRISA
-	
-	bcf		TRISA, 0
-	clrf		ANSEL	
-
-;Setup comparator
-;CMCON to XX0XX101
-;Independent comparator 2, 1 is off
 	banksel	CMCON
-	
-	movlw	b'00100101'
+	movlw	b'00000101'
 	movwf	CMCON
 
 Loop
-	banksel	CMCON
+	banksel 	CMCON
 	btfss	CMCON, 7
-	 goto	TurnLedOff
-TurnLedOn
-	banksel	PORTA
-	bsf		PORTA, 0
+	 goto	LedOff
+LedOn
+	call 	TurnLedOn
 	goto		Loop
-	
-TurnLedOff
-	banksel	PORTA
-	bcf		PORTA, 0
+LedOff
+	call		TurnLedOff
 	goto		Loop
-
-;Synchronize
-;Get sample
-;Process sample
-;Store in program memory at a location
 
 	end
