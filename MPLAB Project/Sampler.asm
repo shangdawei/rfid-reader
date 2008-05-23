f40c532d924ce92538c2aaf7172aec92c14ffd4e
pacemkr <pacemkr@4c31db07-994d-0410-8d14-35da9c29b421>
2008-05-23 21:29:53 +0000

    High speed sampler implemented.

--- MPLAB Project/Sampler.asm
+++ MPLAB Project/Sampler.asm
@@ -5,22 +5,56 @@
 
 Sampler	code
 
+SampleAndStore	macro targetRegister
+		btfsc	CMCON, 7
+		 bsf		targetRegister, 7
+		btfsc	CMCON, 7
+		 bsf		targetRegister, 6
+		btfsc	CMCON, 7
+		 bsf		targetRegister, 5
+		btfsc	CMCON, 7
+		 bsf		targetRegister, 4
+		btfsc	CMCON, 7
+		 bsf		targetRegister, 3
+		btfsc	CMCON, 7
+		 bsf		targetRegister, 2
+		btfsc	CMCON, 7
+		 bsf		targetRegister, 1
+		btfsc	CMCON, 7
+		 bsf		targetRegister, 0
+			endm
+
 SampleEncodedBits
 	global SampleEncodedBits
 
+	; Set up the comparator
 	banksel	CMCON
 	movlw	b'00000101'
 	movwf	CMCON
+	
+	; -------------------
+	; we are at 500 kHz
+	; 8 us per instruction
+	
+	; -------------------
+	; Attempt to synchronize
 
-Loop
-	banksel 	CMCON
+HoldForHighEdge
 	btfss	CMCON, 7
-	 goto	LedOff
-LedOn
-	call 	TurnLedOn
-	goto		Loop
-LedOff
-	call		TurnLedOff
-	goto		Loop
+	 goto	HoldForHighEdge
+
+	; at this point we HAVE a HIGH
+	; start sampling as fast as possible
+	
+	errorlevel 1
+curSampleReg =	0xA0
+	while curSampleReg <= 0xEF
+		SampleAndStore		curSampleReg
+curSampleReg += 0x01
+	endw
+	errorlevel 0
+	
+	return
+	
 
 	end
