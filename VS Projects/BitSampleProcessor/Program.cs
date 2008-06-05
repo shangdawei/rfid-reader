90a5e210cfca9bb105f7a3aaaf3bd04ff7a2a853
pacemkr <pacemkr@4c31db07-994d-0410-8d14-35da9c29b421>
2008-06-05 17:21:55 +0000

    Sampler now allows for a selection of an acceptable error range when measuring the period of the signal. Samples are being stored in memory address 0x110 through 0x16E. BitSampleProcessor updated to extract useful information, such as the number of consecutive bits, and the frequency with which each stream of consecutive bits occurs.

--- VS Projects/BitSampleProcessor/Program.cs
+++ VS Projects/BitSampleProcessor/Program.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Diagnostics;
 
 namespace BitSampleProcessor
 {
@@ -15,6 +18,29 @@ namespace BitSampleProcessor
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+        }
+
+        internal static string ProcessAndOpen( string rawFilePath )
+        {
+            SampleProcessor sampleProcessor = new SampleProcessor();
+            sampleProcessor.LoadFromFile(new FileInfo(rawFilePath));
+
+            sampleProcessor.BreakPlacingDigitSequencePerLine();
+            sampleProcessor.PlaceCharacterCountInFirstColumn();
+            sampleProcessor.AddCountFrequencyAtEnd();
+
+            Regex matchFileName = new Regex(@"(.+)(\.txt)$", RegexOptions.IgnoreCase);
+            string processedFilePath = matchFileName.Replace(rawFilePath, "$1_processed$2");
+            
+            sampleProcessor.SaveToFile(processedFilePath);
+            Program.OpenProcessedFile(processedFilePath);
+
+            return processedFilePath;
+        }
+
+        internal static void OpenProcessedFile(string fileName)
+        {
+            Process.Start(fileName);
         }
     }
 }
--- VS Projects/BitSampleProcessor/SampleProcessor.cs
+++ VS Projects/BitSampleProcessor/SampleProcessor.cs
@@ -29,12 +29,52 @@ namespace BitSampleProcessor
             rawData = matchSequences.Replace(rawData, @"$1" + Environment.NewLine);
         }
 
+        public void PlaceCharacterCountInFirstColumn()
+        {
+            StringReader reader = new StringReader(this.rawData);
+            StringWriter writer = new StringWriter();
+            
+            string line; 
+            while ((line = reader.ReadLine()) != null)
+            {
+                writer.WriteLine(line.Length + "\t" + line);
+            }
+
+            this.rawData = writer.GetStringBuilder().ToString();
+        }
+
         public void SaveToFile(string filePath)
         {
             using (StreamWriter sw = new StreamWriter(filePath))
             {
                 sw.Write(this.rawData);
             }            
+        }
+
+        internal void AddCountFrequencyAtEnd()
+        {
+            Dictionary<string, int> frequencyCount = new Dictionary<string, int>();
+            Regex matchFirstColumn = new Regex("(.+)\\t");
+            foreach (Match count in matchFirstColumn.Matches(this.rawData))
+            {
+                if (frequencyCount.ContainsKey(count.Value))
+                    frequencyCount[count.Value]++;
+                else
+                    frequencyCount.Add(count.Value, 1);
+            }
+
+            StringBuilder builder = new StringBuilder(this.rawData);
+            builder.Append(Environment.NewLine);
+            builder.AppendLine("Count\tFrequency");
+            foreach (string count in frequencyCount.Keys)
+            {
+                builder.Append(count);
+                builder.Append("\t");
+                builder.Append(frequencyCount[count]);
+                builder.Append(Environment.NewLine);
+            }
+
+            this.rawData = builder.ToString();
         }
     }
 }
