f363e9ae3c7f19a9f46add6998913c12682ec9f7
pacemkr <pacemkr@4c31db07-994d-0410-8d14-35da9c29b421>
2008-08-15 01:41:07 +0000

    More work on the tag sampler. This version is not using a timer to cause an interrupt every 400 us. It is attempting to calculate the delay value. The calculated value is incorrect.

--- MPLAB Project/TagSampler.asm
+++ MPLAB Project/TagSampler.asm
@@ -1,10 +1,12 @@
 	#include <p16f88.inc>
-	extern	TurnLedOff
 	extern	TurnLedOn
+	extern	TurnLedOff
 
-TagSamplerData idata
-Flags	res	.1
-Bit		equ	1
+TagDataBuffer idata 0x110 
+BitsLeftInCurrentByte	db	.8	
+BitBuffer  			res 	.94
+LastAddrInBuffer		equ 	0x6F
+
 
 TagSamplerCode code
 
@@ -35,27 +37,28 @@ GetBit
 	; Anything slower than 12.5 kHz is our mark frequency, everything else is space
 
 	; measure period
-	WaitForComparatorLowToHighEdge
+	WaitForComparatorLowToHighEdge ; 4 cycles
 
-	banksel	TMR0
-	clrf		TMR0		; TMR0 will start in 2 instructions (1 us) as per 16f88 datasheet (page 67)
+	banksel	TMR0		; 2 cycles
+	clrf		TMR0		; 3 cycles, TMR0 will start in 2 cycles (1 us) as per 16f88 datasheet (page 67)
 
-	WaitForComparatorLowToHighEdge
+	WaitForComparatorLowToHighEdge ; 1 cycle, possible and will result in the smallest timer value
 	
-	banksel	TMR0
-	movfw	TMR0
+	banksel	TMR0		; 2 cycles
+	movfw	TMR0		; 1 cycle
 
 	; At 12.5k the min timer value:
-	;		80 - 2 = 78 us * .5 us per inst = 156
-	; Anything 78 us (156 instructions) or slower is mark
+	;		80 us = 160 cycles
+	;		160 cycles - 13 cycles (min overhead) = 147
+	; Anything 147 cycles or slower is mark
 
-	addlw	.255 - .157
-	bc		SetBitHigh	; timer value > 157
-	bcf		Flags, Bit	; timer value <= 157
+	addlw	.255 - .146
+	bc		SetBitHigh	; timer value > 146
+	bcf		STATUS, C	; timer value <= 146
 	nop		; pad the timing
 	goto		ReturnFromGetBit
 SetBitHigh
-	bsf		Flags, Bit	
+	bsf		STATUS, C	
 
 ReturnFromGetBit										
 	return
@@ -63,33 +66,78 @@ ReturnFromGetBit
 
 ;******************************************************************************
 
-HoldForLowBit
-	
+SyncWithFirstBitOfTag
+
+HoldForLowBit	
 	call 	GetBit
-	btfsc	Flags, Bit
+	btfsc	STATUS, C
 	 goto	HoldForLowBit
 
-	return
-
-
-;******************************************************************************
-
 HoldForHighBit
-	
 	call 	GetBit
-	btfss	Flags, Bit
+	btfss	STATUS, C
 	 goto	HoldForHighBit
 
+	; wait half bit time to put next call at 200 us after edge
+
 	return
-	
+
 
 ;******************************************************************************
 
-SyncWithLowToHighBitEdge
+RecordBitAndWaitForNext
 	
-	call	HoldForLowBit
-	call HoldForHighBit
+	; keep track of how long it will take to process this bit
+	banksel	TMR2		; 2 cycles
+	clrf 	TMR2		; 1 cycle
+
+	; debug pulse, should see it every 400 us (bit time) sharp!
+	call		TurnLedOn
+	call		TurnLedOff
+
+	call 	GetBit
+	rlf		INDF, f
+		
+	decfsz	BitsLeftInCurrentByte, f
+	 goto 	WaitForNextBit
+	; set up for next byte in buffer
+	incf		FSR, f
+	movfw	FSR
+	; check if buffer is full
+	addlw	.255 - LastAddrInBuffer	
+	bc		BufferFull ; If FSR > LastAddrInBuffer
+	; reinitialize the "bits left" value, since we are at the next, empty byte
+	movlw	.8
+	movwf	BitsLeftInCurrentByte
+
+WaitForNextBit
+	; put the next call to GetBit at 200 us (400 cycles) from last
+DelayValue	db	0
+ElapsedCycles	db 	0
+
+	movfw	TMR2			; 1 cycle
+	movwf	ElapsedCycles	; 1 cycle
 	
+	; 145 cycles
+	movlw	0x30
+	movwf	DelayValue
+Delay_0
+	decfsz	DelayValue, f
+	goto	Delay_0
+
+	; [255 - overhead - bit processing time (from TMR2)] cycles
+	; WRONG DELAY!!
+	movlw	.81
+	movwf	DelayValue
+	movfw	ElapsedCycles
+	subwf	DelayValue, f
+Delay_1
+	decfsz	DelayValue, f
+	goto	Delay_1
+	
+	goto 	RecordBitAndWaitForNext	; 2 cycles
+
+BufferFull
 	return
 
 
@@ -99,7 +147,7 @@ WaitForTagAndReadRawData
 	global	WaitForTagAndReadRawData
 
 	;-------------------------------------------------------------------------
-	; Set up
+	; Set up the comparator, timers, and indirect addressing
 	;
 													errorlevel -302
 	; Set up the comparator for independent mode
@@ -109,25 +157,37 @@ WaitForTagAndReadRawData
 	movwf	CMCON
 													errorlevel +302
 
-	; Set up the timer
-	; 	- disable timer interrupt
-	; 	- use instruction clock
-	; 	- assign postscaler to WDT
+	; Disable timer interrupt
 	bcf		INTCON, 5
+
+	; TMR0, assign postscaler to WDT, use instruction clock
 	bcf		OPTION_REG, 5
-	bsf		OPTION_REG, 3	
+	bsf		OPTION_REG, 3
+
+	; TMR2
+	banksel	T2CON
+	movlw	b'00000100'
+	movwf	T2CON		; turn on the timer, no pre/post scalers
+
+	banksel	PR2
+	movlw	0xFF
+	movwf	PR2			; set timer period to 255
+	bcf		PIE1, TMR2IE 	; disable interrupt
+	
+
+	; Select the correct bank for indirect addressing
+	bsf		STATUS, IRP
+	; Initialize the FSR to point to TagData for indirect addressing
+	movlw	BitBuffer
+	movwf	FSR
+
 
 	;-------------------------------------------------------------------------
-	; Logic
+	; Read the tag
 	;
 
-TestGetBit
-	call		GetBit
-	btfsc	Flags, Bit
-	 call	TurnLedOff
-	btfss	Flags, Bit
-	 call	TurnLedOn
-	goto		TestGetBit
+	call 	SyncWithFirstBitOfTag
+	call 	RecordBitAndWaitForNext
 
 	return
 
