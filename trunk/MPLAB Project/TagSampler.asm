ff3f281745c7d36e696ced46f299b964ce545e68
pacemkr <pacemkr@4c31db07-994d-0410-8d14-35da9c29b421>
2008-08-15 03:15:17 +0000

    This tag sampler is using a TMR2 interrupt to store a bit every 400 us.

--- trunk/MPLAB Project/TagSampler.asm
+++ trunk/MPLAB Project/TagSampler.asm
@@ -1,6 +1,8 @@
 	#include <p16f88.inc>
 	extern	TurnLedOn
 	extern	TurnLedOff
+	
+	errorlevel	-302
 
 TagDataBuffer idata 0x110 
 BitsLeftInCurrentByte	db	.8	
@@ -17,7 +19,7 @@ WaitForComparatorLowToHighEdge	macro
 	local HoldForHigh
 											
 		banksel	CMCON
-											errorlevel	-207, -302
+												errorlevel	-207
 	HoldForLow
 		btfsc	CMCON, 7
 		 goto	HoldForLow
@@ -25,7 +27,7 @@ WaitForComparatorLowToHighEdge	macro
 	HoldForHigh
 		btfss	CMCON, 7
 		 goto	HoldForHigh
-											errorlevel	+207, +302
+												errorlevel	+207
 	endm	; At this point we are high for at most 4 instructions (2 us)
 	
 
@@ -85,12 +87,9 @@ HoldForHighBit
 
 ;******************************************************************************
 
-RecordBitAndWaitForNext
+StoreBit
+	global	StoreBit
 	
-	; keep track of how long it will take to process this bit
-	banksel	TMR2		; 2 cycles
-	clrf 	TMR2		; 1 cycle
-
 	; debug pulse, should see it every 400 us (bit time) sharp!
 	call		TurnLedOn
 	call		TurnLedOff
@@ -99,45 +98,26 @@ RecordBitAndWaitForNext
 	rlf		INDF, f
 		
 	decfsz	BitsLeftInCurrentByte, f
-	 goto 	WaitForNextBit
+	 goto 	BitStored
+	
 	; set up for next byte in buffer
 	incf		FSR, f
 	movfw	FSR
+	
 	; check if buffer is full
 	addlw	.255 - LastAddrInBuffer	
 	bc		BufferFull ; If FSR > LastAddrInBuffer
+	
 	; reinitialize the "bits left" value, since we are at the next, empty byte
 	movlw	.8
 	movwf	BitsLeftInCurrentByte
-
-WaitForNextBit
-	; put the next call to GetBit at 200 us (400 cycles) from last
-DelayValue	db	0
-ElapsedCycles	db 	0
-
-	movfw	TMR2			; 1 cycle
-	movwf	ElapsedCycles	; 1 cycle
-	
-	; 145 cycles
-	movlw	0x30
-	movwf	DelayValue
-Delay_0
-	decfsz	DelayValue, f
-	goto	Delay_0
-
-	; [255 - overhead - bit processing time (from TMR2)] cycles
-	; WRONG DELAY!!
-	movlw	.81
-	movwf	DelayValue
-	movfw	ElapsedCycles
-	subwf	DelayValue, f
-Delay_1
-	decfsz	DelayValue, f
-	goto	Delay_1
-	
-	goto 	RecordBitAndWaitForNext	; 2 cycles
-
+		
+	goto		BitStored			
 BufferFull
+	banksel	PIE1
+	bcf		PIE1, TMR2IE 	; disable timer interrupt	
+		
+BitStored
 	return
 
 
@@ -149,31 +129,19 @@ WaitForTagAndReadRawData
 	;-------------------------------------------------------------------------
 	; Set up the comparator, timers, and indirect addressing
 	;
-													errorlevel -302
+
 	; Set up the comparator for independent mode
 	; Using two external pins, RA1 and RA2
 	banksel	CMCON
 	movlw	b'00000101'
 	movwf	CMCON
-													errorlevel +302
+													
 
 	; Disable timer interrupt
 	bcf		INTCON, 5
-
 	; TMR0, assign postscaler to WDT, use instruction clock
 	bcf		OPTION_REG, 5
-	bsf		OPTION_REG, 3
-
-	; TMR2
-	banksel	T2CON
-	movlw	b'00000100'
-	movwf	T2CON		; turn on the timer, no pre/post scalers
-
-	banksel	PR2
-	movlw	0xFF
-	movwf	PR2			; set timer period to 255
-	bcf		PIE1, TMR2IE 	; disable interrupt
-	
+	bsf		OPTION_REG, 3	
 
 	; Select the correct bank for indirect addressing
 	bsf		STATUS, IRP
@@ -187,8 +155,20 @@ WaitForTagAndReadRawData
 	;
 
 	call 	SyncWithFirstBitOfTag
-	call 	RecordBitAndWaitForNext
+
+	banksel	PR2
+	movlw	.200
+	movwf	PR2			; will cause an interrupt in 800 cycles
+	bsf		INTCON, GIE	; enable interrupts
+	bsf		PIE1, TMR2IE 	; enable timer interrupt	
+
+	banksel	T2CON
+	movlw	b'00010100'
+	movwf	T2CON		; turn on the timer, 1:4 postscaler
+
+WaitForInterrupt
+	goto WaitForInterrupt
 
 	return
 
-end
+	end
